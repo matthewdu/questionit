096488bb9c5e5967d0cef82ea1702893b54043be
Jason Loo <jmloo@uwaterloo.ca>
2014-08-08 22:59:06 -0400

    add index to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
 	# Save a downcased version of the username for login
 	before_save { self.username = username.downcase }
-	validates(:username, presence: true, length: { maximum: 50 }, uniqueness: true )
+	validates(:username, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false } )
 
 
 	has_secure_password
