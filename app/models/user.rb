5f361be6403b0e998653b47103d1e932cdadbbf4
Jason Loo <jmloo@uwaterloo.ca>
2014-08-08 22:55:32 -0400

    Code Review Comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 
 	# Save a downcased version of the username for login
-	before_save { self.duser = username.downcase }
-	validates(:username, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false } )
+	before_save { self.username = username.downcase }
+	validates(:username, presence: true, length: { maximum: 50 }, uniqueness: true )
 
 
 	has_secure_password
--- db/migrate/20140809023920_add_user_columns_to_users.rb
+++ db/migrate/20140809023920_add_user_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddUserColumnsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :duser, :string
+    add_column :users, :name, :string
     add_column :users, :password_digest, :string
   end
 end
