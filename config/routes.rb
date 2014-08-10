271d6b702ce00047d3390dba4d1ac52ffeb2513f
Jason Loo <jmloo@uwaterloo.ca>
2014-08-10 02:40:31 -0400

    Login view, action, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/about',	to: 'static_pages#about',   via: 'get'
+
   resources :users
-  match '/signup', to: 'users#new',   via: 'get'
+  match '/signup',	to: 'users#new',   			via: 'get'
+
+  match '/signin',	to: 'sessions#new',			via: 'get'
+  match '/signout', to: 'sessions#destroy',		via: 'delete'
 end
