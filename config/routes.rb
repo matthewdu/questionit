87f8edc57c3479746b108d3ab47e1162260b4490
Jason Loo <jmloo@uwaterloo.ca>
2014-08-10 03:14:37 -0400

    Edit header and add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   match '/signup',	to: 'users#new',   			via: 'get'
 
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signin',	to: 'sessions#new',			via: 'get'
   match '/signout', to: 'sessions#destroy',		via: 'delete'
 end
