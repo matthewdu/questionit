384d3548c1fd3936d06bce843881a4f58a3b1c13
Jason Loo <jmloo@uwaterloo.ca>
2014-08-10 20:55:54 -0400

    Signed in and correct user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -40,5 +43,14 @@ class UsersController < ApplicationController
 
 		def settings_params
 			params.require(:user).permit(:name, :password, :password_confirmation)
+		end
+
+		def signed_in_user
+			redirect_to signin_url, notice: "Please Sign in." unless signed_in?
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
 		end
 end
