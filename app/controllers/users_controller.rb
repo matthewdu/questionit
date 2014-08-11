8b914119b592f7dc81a87f0b5c718335d82ccff8
Jason Loo <jmloo@uwaterloo.ca>
2014-08-10 21:53:22 -0400

    Move methods to helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,20 +55,4 @@ class UsersController < ApplicationController
 		def settings_params
 			params.require(:user).permit(:name, :password, :password_confirmation)
 		end
-
-		def signed_in_user
-			unless signed_in?
-				store_location
-				redirect_to signin_url, notice: "Please Sign in." unless signed_in?
-			end
-		end
-
-		def correct_user
-			@user = User.find(params[:id])
-			redirect_to(root_url) unless current_user?(@user)
-		end
-
-		def admin_user
-			redirect_to(root_url) unless current_user.admin?
-		end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,6 +25,17 @@ module SessionsHelper
 		user == current_user
 	end
 
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_url, notice: "Please Sign in." unless signed_in?
+		end
+	end
+
+	def admin_user
+		redirect_to(root_url) unless current_user.admin?
+	end
+
 	def sign_out
 		current_user.update_attribute(:remember_token, User.digest(User.new_remember_token))
 		cookies.delete(:remember_token)
@@ -40,4 +51,8 @@ module SessionsHelper
 		session[:return_to] = request.url if request.get?
 	end
 
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless current_user?(@user)
+	end
 end
