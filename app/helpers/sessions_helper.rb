14c5df79930b051bb723fea984f7fd77cf67e917
Matthew Du <du.matthew@gmail.com>
2014-08-19 23:08:50 -0400

    fix regression bug from previous commit allowing unsigned in users to view user profiles

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -28,7 +28,8 @@ module SessionsHelper
 	def signed_in_user
 		unless signed_in?
 			store_location
-			redirect_to signin_url, notice: "Please Sign in." unless signed_in?
+			flash[:danger] = 'Please sign in'
+			redirect_to signin_url
 		end
 	end
 
