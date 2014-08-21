83aba1b994e4b1de19eabce32023d6995d9eece4
Jason Loo <jmloo@uwaterloo.ca>
2014-08-21 02:10:50 -0400

    form syntax for nested resources

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,18 @@
 class QuestionsController < ApplicationController
 	def new
+		@answer = Answer.find(params[:answer_id])
 		@question = Question.new
 	end
 
 	def create
 		@answer = Answer.find(params[:answer_id])
-		question = @answer.questions.new(question_params)
+		@question = @answer.questions.new(question_params)
+		if @question.save
+			flash[:success] = "Question Saved"
+			redirect_to @answer
+		else
+			render 'new'
+		end
 	end
 
 	private
