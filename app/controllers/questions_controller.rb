41598c5663c32f876fab857391e0651b8d5fa1cc
Matthew Du <du.matthew@gmail.com>
2014-08-21 23:05:07 -0400

    add questions#edit #update #destroy and add new question to answer#show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,8 @@
 class QuestionsController < ApplicationController
+	before_action :signed_in_user, only: [:new, :create, :edit, :update, :destroy]
+
 	def new
+		# currently not used
 		@answer = Answer.find(params[:answer_id])
 		@question = Question.new
 	end
@@ -7,18 +10,45 @@ class QuestionsController < ApplicationController
 	def create
 		@answer = Answer.find(params[:answer_id])
 		@question = @answer.questions.new(question_params)
+		@question.user_id = current_user.id
 		if @question.save
 			flash[:success] = "Question Saved"
 			redirect_to @answer
 		else
-			render 'new'
+			render 'answers/show'
+		end
+	end
+
+	def edit
+		@question = Question.find(params[:id])
+	end
+
+	def update
+		question = Question.find(params[:id])
+		render :edit unless current_user.admin? || current_user?(@question.user)
+		if question.update_attributes(question_params)
+			flash[:success] = 'Question updated!'
+			redirect_to question.answer
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		question = Question.find(params[:id])
+		if current_user.admin? || current_user?(@question.user)
+			question.destroy
+			flash[:success] = 'Question destroyed!'
+			redirect_to question.answer
+		else
+			flash[:danger] = 'No permission to delete'
+			redirect_to question.answer
 		end
 	end
 
 	private
 		def question_params
-			params[:user_id] = current_user.id
-			params.require(:question).permit(:content, :user_id)
+			params.require(:question).permit(:content)
 		end
 
 end
