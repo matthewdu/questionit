3b21e4218f2fafd1641a4c78584031f8b5ec479e
Matthew Du <du.matthew@gmail.com>
2014-08-28 00:44:55 -0400

    fix answer bug after removing points

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,8 +5,6 @@ class Answer < ActiveRecord::Base
 	belongs_to :user
 	has_many :questions
 
-	before_save { self.points = 1 }
-
 	validates :title, presence: true, length: { maximum: 255 }
 	validates :content, presence: true
 
